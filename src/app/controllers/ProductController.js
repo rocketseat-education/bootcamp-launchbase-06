e83825a99fe8092f77fc9547322d4590d4655921
Bootcamp Maykao <mayk@rocketseat.com.br>
2019-12-08 17:19:25 -0400

    11 Update e Delete de Products

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -46,5 +46,30 @@ module.exports = {
 
 
         return res.render("products/edit.njk", { product, categories })
+    },
+    async put(req, res) {
+        const keys = Object.keys(req.body)
+
+        for(key of keys) {
+            if (req.body[key] == "") {
+                return res.send('Please, fill all fields!')
+            }
+        }
+
+        req.body.price = req.body.price.replace(/\D/g, "")
+
+        if (req.body.old_price != req.body.price) {
+            const oldProduct = await Product.find(req.body.id)
+            req.body.old_price = oldProduct.rows[0].price
+        }
+
+        await Product.update(req.body)
+
+        return res.redirect(`/products/${req.body.id}/edit`)
+    },
+    async delete(req, res) {
+        await Product.delete(req.body.id)
+
+        return res.redirect('/products/create')
     }
 }
--- src/routes.js
+++ src/routes.js
@@ -10,6 +10,8 @@ routes.get('/products/create', ProductController.create)
 routes.get('/products/:id/edit', ProductController.edit)
 
 routes.post('/products', ProductController.post)
+routes.put('/products', ProductController.put)
+routes.delete('/products', ProductController.delete)
 
 // Alias
 routes.get('/ads/create', function(req, res) {
