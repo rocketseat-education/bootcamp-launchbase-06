5da3af60d7a492568b28b68e2a9b4272e945eb1b
Bootcamp Maykao <mayk@rocketseat.com.br>
2019-12-08 13:26:24 -0400

    07 Promises de Categorias

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -1,5 +1,20 @@
+const Category = require('../models/Category')
+
 module.exports = {
     create(req, res) {
-        return res.render("products/create.njk")
+        // Pegar Categorias
+        Category.all()
+        .then(function(results) {
+
+            const categories = results.rows
+
+            return res.render("products/create.njk", { categories })
+        }).catch(function(err) {
+            throw new Error(err)
+        })
+
+    },
+    post(req, res) {
+        // Logica de Salvar
     }
 }
--- src/routes.js
+++ src/routes.js
@@ -7,8 +7,9 @@ routes.get('/', function(req, res) {
 })
 
 routes.get('/products/create', ProductController.create)
+routes.post('/products', ProductController.post)
 
-
+// Alias
 routes.get('/ads/create', function(req, res) {
     return res.redirect("/products/create")
 })
