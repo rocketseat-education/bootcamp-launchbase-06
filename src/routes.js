44a27308c17c3d640e6cae5cd4bb73f5a22a285e
Bootcamp Maykao <mayk@rocketseat.com.br>
2019-12-08 13:02:16 -0400

    05 HTML do Cadastro de Produtos 06 Estilizando o Cadastro de Produtos

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,17 @@
 const express = require('express')
 const routes = express.Router()
+const ProductController = require('./app/controllers/ProductController')
 
 routes.get('/', function(req, res) {
     return res.render("layout.njk")
 })
 
+routes.get('/products/create', ProductController.create)
+
+
+routes.get('/ads/create', function(req, res) {
+    return res.redirect("/products/create")
+})
 
 
 module.exports = routes
