70ecef62b1cf213cdbeea0449dabd3c0f855cec2
Bootcamp Maykao <mayk@rocketseat.com.br>
2019-12-08 19:58:13 -0400

    17 Salvando Imagens dos Produtos

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -2,6 +2,7 @@ const { formatPrice } = require('../../lib/utils')
 
 const Category = require('../models/Category')
 const Product = require('../models/Product')
+const File = require('../models/File')
 
 module.exports = {
     create(req, res) {
@@ -24,10 +25,17 @@ module.exports = {
             }
         }
 
+        if (req.files.length == 0)
+            return res.send('Please, send at least one image')
+
+
         let results = await Product.create(req.body)
         const productId = results.rows[0].id
 
-        return res.redirect(`/products/${productId}`)
+        const filesPromise = req.files.map(file => File.create({...file, product_id: productId}))
+        await Promise.all(filesPromise)
+
+        return res.redirect(`/products/${productId}/edit`)
 
     },
     async edit(req, res) {
