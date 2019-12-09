ee31f1751d6cd6a86e1cfced6cd1391fba79b7f0
Bootcamp Maykao <mayk@rocketseat.com.br>
2019-12-09 09:53:41 -0400

    28 Estruturando Home

--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,9 @@ const routes = express.Router()
 const multer = require('./app/middlewares/multer')
 
 const ProductController = require('./app/controllers/ProductController')
+const HomeController = require('./app/controllers/HomeController')
 
-routes.get('/', function(req, res) {
-    return res.render("layout.njk")
-})
+routes.get('/', HomeController.index)
 
 routes.get('/products/create', ProductController.create)
 routes.get('/products/:id', ProductController.show)
