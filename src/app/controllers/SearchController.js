8e1ab2b8532deb08c9cbcc62e955aed37f4ae6d2
Bootcamp Maykao <mayk@rocketseat.com.br>
2019-12-09 11:47:16 -0400

    32 Filtragem da Search Page

--- src/app/controllers/SearchController.js
+++ src/app/controllers/SearchController.js
@@ -5,10 +5,20 @@ const Product = require('../models/Product')
 module.exports = { 
     async index(req, res) {
         try {
-            let results = await Product.all()
-            const products = results.rows
+            let results,
+                params = {}
 
-            if (!products) return res.send("Products not found!")
+            const { filter, category } = req.query
+
+            if (!filter) return res.redirect("/")
+
+            params.filter = filter
+
+            if (category) {
+                params.category = category
+            }
+
+            results = await Product.search(params)
 
             async function getImage(productId) {
                 let results = await Product.files(productId)
@@ -17,25 +27,46 @@ module.exports = {
                 return files[0]
             }
 
-            const productsPromise = products.map(async product => {
+            const productsPromise =  results.rows.map(async product => {
                 product.img = await getImage(product.id)
                 product.oldPrice = formatPrice(product.old_price)
                 product.price = formatPrice(product.price)
                 return product
-            }).filter((product, index) => index > 2 ? false : true)
-            
+            })
 
-            const lastAdded = await Promise.all(productsPromise)
+            const products = await Promise.all(productsPromise)
 
-            return res.render("search/index", { products: lastAdded })
+
+            const search = {
+                term: req.query.filter,
+                total: products.length
+            }
+
+
+
+            const categories = products.map(product => ({
+                id: product.category_id,
+                name: product.category_name
+            })).reduce((categoriesFiltered, category) => {
+
+                const found = categoriesFiltered.some(cat => cat.id == category.id)
+
+                if (!found)
+                    categoriesFiltered.push(category)
+
+                return categoriesFiltered
+            }, [])
+
+
+
+
+            return res.render("search/index", { products, search, categories })
 
 
         }
         catch(err) {
             console.error(err)
         }
-        
-
         
     }
 }
--- src/app/models/Product.js
+++ src/app/models/Product.js
@@ -74,6 +74,34 @@ module.exports = {
         return db.query(`
             SELECT * FROM files WHERE product_id = $1
         `, [id])
+    },
+    search(params) {
+        const { filter, category } = params
+
+        let query = "",
+            filterQuery = `WHERE`
+
+        if (category) {
+            filterQuery = `${filterQuery}
+            products.category_id = ${category}
+            AND`
+        }
+
+        filterQuery = `
+            ${filterQuery}
+            products.name ilike '%${filter}%'
+            OR products.description ilike '%${filter}%'
+        `
+
+        query = `
+            SELECT products.*,
+                categories.name AS category_name
+            FROM products
+            LEFT JOIN categories ON (categories.id = products.category_id)
+            ${filterQuery}
+        `
+
+        return db.query(query)
     }
 
 }
