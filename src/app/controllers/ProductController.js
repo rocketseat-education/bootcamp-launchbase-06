4e733e350636a343eb8d8a735dde50a910987459
Bootcamp Maykao <mayk@rocketseat.com.br>
2019-12-08 21:18:58 -0400

    19 Exclusão de imagens do Banco e no Gerenciador

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -66,10 +66,24 @@ module.exports = {
         const keys = Object.keys(req.body)
 
         for(key of keys) {
-            if (req.body[key] == "") {
+            if (req.body[key] == "" && key != "removed_files") {
                 return res.send('Please, fill all fields!')
             }
         }
+
+
+        if (req.body.removed_files) {
+            // 1,2,3,
+            const removedFiles = req.body.removed_files.split(",") // [1,2,3,]
+            const lastIndex = removedFiles.length - 1
+            removedFiles.splice(lastIndex, 1) // [1,2,3]
+
+            const removedFilesPromise = removedFiles.map(id => File.delete(id))
+
+            await Promise.all(removedFilesPromise)
+        }
+
+
 
         req.body.price = req.body.price.replace(/\D/g, "")
 
